535f11ce7554d269aacbea7b2788b103429c366d
Yanling Zhu <u6579751@anu.edu.au>
2019-10-05 00:59:44 +1000

    Update App.java

--- TODOLIST/app/src/main/java/com/comp2100/todolist/App.java
+++ TODOLIST/app/src/main/java/com/comp2100/todolist/App.java
@@ -1,19 +1,22 @@
 package com.comp2100.todolist;
 
-import android.app.Application;
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationManagerCompat;
 
-public class App extends Application {
+public class App extends Activity {
     public static final String CHANNEL_1_ID = "channel1";
     public static final String CHANNEL_2_ID = "channel2";
     @Override
-    public void onCreate(){
-        super.onCreate();
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         creatNotificationChannels();
 
 
@@ -21,11 +24,12 @@ public class App extends Application {
 
     private void creatNotificationChannels() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.BASE) {
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
             NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID,"Channel 1", NotificationManager.IMPORTANCE_DEFAULT);
             channel1.setDescription("This is channel 1");
             NotificationChannel channel2 = new NotificationChannel(CHANNEL_2_ID,"Channel 2",NotificationManager.IMPORTANCE_DEFAULT);
             channel2.setDescription("This is channel 2");
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel1);
             notificationManager.createNotificationChannel(channel2);
         }
