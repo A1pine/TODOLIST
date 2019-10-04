c2522323ae6e4c9cbedd1847f86125dc501f487c
Yanling Zhu <u6579751@anu.edu.au>
2019-10-05 00:58:43 +1000

    Update Alertreceive_JavaClass.java

--- TODOLIST/app/src/main/java/com/comp2100/todolist/Alertreceive_JavaClass.java
+++ TODOLIST/app/src/main/java/com/comp2100/todolist/Alertreceive_JavaClass.java
@@ -1,15 +1,17 @@
 package com.comp2100.todolist;
 
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationManagerCompat;
 
-class AlertReceive {
+class AlertReceive extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent){
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-        NotificationHelper notificationHelper = new NotificationHelper(context);
-        notificationManagerCompat.notify(1,notificationHelper.getBuilder().build());
+        NotificationHelper notificationHelper = new NotificationHelper();
+        notificationHelper.setBuilder(context,"this is the time","!!!!!!");
+        notificationManagerCompat.notify(1,notificationHelper.getNotification());
     }
 }
