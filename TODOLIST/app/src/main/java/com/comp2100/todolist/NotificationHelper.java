392dbcfa0332ba72955980260ad8520138f1c8ba
Yanling Zhu <u6579751@anu.edu.au>
2019-10-05 01:00:30 +1000

    Update NotificationHelper.java

--- TODOLIST/app/src/main/java/com/comp2100/todolist/NotificationHelper.java
+++ TODOLIST/app/src/main/java/com/comp2100/todolist/NotificationHelper.java
@@ -1,17 +1,25 @@
 package com.comp2100.todolist;
 
 import android.content.Context;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Notification;
 import androidx.core.app.NotificationCompat;
+
 
 public class NotificationHelper {
     NotificationCompat.Builder builder;
-    public NotificationHelper(Context context){
-        builder = new NotificationCompat.Builder(context, App.CHANNEL_1_ID).setSmallIcon(R.drawable.ic_star)
-                .setContentTitle("My notification").setContentText("Alarm!").setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+    void setBuilder (Context context, String title, String description){
+        builder = new NotificationCompat.Builder(context, App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable. ic_priority_high_black_24dp)
+                .setContentTitle("this is notification")
+                .setContentText("ok...")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("fine..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
-    public NotificationCompat.Builder getBuilder(){
-        return builder;
+    Notification getNotification(){
+       return builder.build();
+
     }
+
 }
